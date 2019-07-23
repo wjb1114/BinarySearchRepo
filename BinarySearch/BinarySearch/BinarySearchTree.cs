3a88c6397aec9d1748db4661205888f98a4222c2
wjb1114 <kbd1114@gmail.com>
2019-07-23 11:45:00 -0500

    Added BinarySearchTree and Node classes. Implemented BinarySearchTree.Add method.

---
+++ BinarySearch/BinarySearch/BinarySearchTree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class BinarySearchTree
+    {
+        public Node root;
+
+        public BinarySearchTree(Node rootNode)
+        {
+            root = rootNode;
+        }
+
+        public BinarySearchTree(int rootValue)
+        {
+            root = new Node(rootValue);
+        }
+
+        public void Add(int value)
+        {
+            Node foundNode = TraverseTreeToAdd(root, value);
+            if (foundNode.value < value)
+            {
+                foundNode.right = new Node(value);
+            }
+            else
+            {
+                foundNode.left = new Node(value);
+            }
+        }
+
+        private Node TraverseTreeToAdd(Node currentNode, int value)
+        {
+            if (value < currentNode.value)
+            {
+                if (currentNode.left != null)
+                {
+                    return TraverseTreeToAdd(currentNode.left, value);
+                }
+                else
+                {
+                    return currentNode;
+                }
+            }
+            else
+            {
+                if (currentNode.right != null)
+                {
+                    return TraverseTreeToAdd(currentNode.right, value);
+                }
+                else
+                {
+                    return currentNode;
+                }
+            }
+        }
+
+        public bool Search(int value)
+        {
+
+        }
+
+        // if (root.left != null)
+    }
+}
---
+++ BinarySearch/BinarySearch/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int number)
+        {
+            value = number;
+        }
+    }
+}
