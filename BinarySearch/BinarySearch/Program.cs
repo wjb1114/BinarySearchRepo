c2019218d43509b65c79f63a25b29582e1d0e99b
wjb1114 <kbd1114@gmail.com>
2019-07-24 09:37:46 -0500

    Performed testing for larger datasets.

--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -11,36 +11,11 @@ namespace BinarySearch
         static void Main(string[] args)
         {
             BinarySearchTree tree = new BinarySearchTree(0);
-            tree.Add(1);
-            tree.Add(2);
-            tree.Add(3);
-            tree.Add(4);
-            tree.Add(5);
-            tree.Add(6);
-            tree.Add(7);
-            tree.Add(8);
-            tree.Add(9);
-            tree.Add(10);
-            tree.Add(11);
-            tree.Add(12);
-            tree.Add(13);
-            tree.Add(14);
-            tree.Add(15);
-            tree.Add(16);
-            tree.Add(17);
-            tree.Add(18);
-            tree.Add(19);
-            tree.Add(20);
-            tree.Add(21);
-            tree.Add(22);
-            tree.Add(23);
-            tree.Add(24);
-            tree.Add(25);
-            tree.Add(26);
-            tree.Add(27);
-            tree.Add(28);
-            tree.Add(29);
-            tree.Add(30);
+            for (int i = 1; i <= 10000; i++)
+            {
+                tree.Add(i);
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
